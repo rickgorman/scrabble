2c0bb7ae2ae67f8914425f9d42855e53fe4ff438
Rick Gorman <gormanite@gmail.com>
2019-07-30 18:36:54 -0400

    implement crux of MoveValidator#valid_move?

--- src/move_validator.rb
+++ src/move_validator.rb
@@ -2,6 +2,8 @@ require_relative './board.rb'
 require_relative './move.rb'
 
 class MoveValidator
+  MIN_WORD_LENGTH = 2
+
   def initialize(board:, move:, dictionary: ['aaa', 'bbb'])
     @board = board
     @move = move
@@ -12,11 +14,7 @@ class MoveValidator
     return false if move_goes_off_the_board?
     return false if move_goes_over_existing_word?
 
-    # create a new board and apply the move
-
-    # scan for all words on the board
-
-    # verify that all words are in the dictionary
+    dictionary_contains_all_words?(words_visible_on(next_board))
   end
 
   private
@@ -49,5 +47,53 @@ class MoveValidator
     end
 
     false
+  end
+
+  def next_board
+    @next_board ||= Board.clone(board)
+
+    # TODO: simplify shared code
+    if move.across?
+      move.letters.each_with_index do |letter, offset|
+        @next_board.set_tile(
+          row: move.row,
+          col: move.col + offset,
+          letter: letter
+        )
+      end
+    elsif move.down?
+      move.letters.each_with_index do |letter, offset|
+        @next_board.set_tile(
+          row: move.row + offset,
+          col: move.col,
+          letter: letter
+        )
+      end
+    end
+
+    @next_board
+  end
+
+  def dictionary_contains_all_words?(words)
+    # TODO: speed this up using a hashmap for the dictionary
+    words.all? { |word| dictionary.include?(word) }
+  end
+
+  def words_visible_on(some_board)
+    # TODO: replace with a better word that describes an abstraction that can be
+    #  either a row or a column.
+    gridlines = []
+
+    # store string representations of all rows and columns
+    some_board.width.times do |idx|
+      gridlines << some_board.get_row(idx).map { |tile| tile.to_s }.join('')
+      gridlines << some_board.get_col(idx).map { |tile| tile.to_s }.join('')
+    end
+
+    # return all words that can be made from these gridlines
+    gridlines
+      .map(&:split)
+      .flatten
+      .select { |word| word.length >= MIN_WORD_LENGTH }
   end
 end
