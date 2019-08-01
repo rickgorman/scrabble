3edf00c0aa0601348b570a856764ddc4a8e788f1
Rick Gorman <gormanite@gmail.com>
2019-08-01 16:18:20 -0400

    add HumanPlayer#receive_tiles method and spec

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -9,4 +9,41 @@ RSpec.describe HumanPlayer do
 
     it { is_expected.to be_a Move }
   end
+
+  describe '#receive_tiles' do
+    let(:player) { HumanPlayer.new(board: Board.new) }
+
+    subject do
+      player.receive_tiles(tiles_to_receive)
+    end
+
+    context 'when the player HAS space in his rack' do
+      let(:tiles_to_receive) { ['a','b','c'] }
+      let(:starting_tiles) { ['x','y','z'] }
+
+      before do
+        player.rack.concat(starting_tiles)
+      end
+
+      it "adds the given tiles to the player's rack" do
+        subject
+
+        expect(player.rack).to match_array (tiles_to_receive + starting_tiles)
+      end
+    end
+
+    context 'when the player DOES NOT have space in his rack' do
+      let(:tiles_to_receive) { ['a','b','c'] }
+      let(:starting_tiles) { ['v','w','x','y','z'] }
+
+      before do
+        player.rack.concat(starting_tiles)
+      end
+
+      it "raises an exception" do
+        expect { subject }.to raise_exception /too many tiles/
+      end
+    end
+  end
+
 end
--- src/human_player.rb
+++ src/human_player.rb
@@ -1,11 +1,22 @@
 require_relative './move.rb'
 
 class HumanPlayer
+  MAX_RACK_SIZE = 7
+
   def initialize(board:)
     @board = board
+    @rack = []
   end
+
+  attr_reader :rack
 
   def get_move
     Move.new(row: 0, col: 0, direction: :down, letters: ['z'])
+  end
+
+  def receive_tiles(tiles)
+    raise "too many tiles in rack" if rack.length + tiles.length > MAX_RACK_SIZE
+
+    rack.concat(tiles)
   end
 end
